bf653926ab37c93496a397c686f90275ba7dd435
ZellosMet <zellossmet@gmail.com>
2024-02-11 22:01:16 +0900

    TaskManager DONE

--- Processes/Form1.Designer.cs
+++ Processes/Form1.Designer.cs
@@ -28,18 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.rtb_ProcessName = new System.Windows.Forms.RichTextBox();
 			this.btn_Start = new System.Windows.Forms.Button();
 			this.btn_Stop = new System.Windows.Forms.Button();
 			this.l_ProcessInfo = new System.Windows.Forms.Label();
 			this.proc_MyProcess = new System.Diagnostics.Process();
+			this.lv_ProcessList = new System.Windows.Forms.ListView();
+			this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Process = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Base_priority = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Priority_class = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Start_time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Total_CPU_time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.User_CPU_time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Session_ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Affinity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Threads = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.t_RefreshInfo = new System.Windows.Forms.Timer(this.components);
 			this.SuspendLayout();
 			// 
 			// rtb_ProcessName
 			// 
+			this.rtb_ProcessName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.rtb_ProcessName.Location = new System.Drawing.Point(12, 13);
 			this.rtb_ProcessName.Name = "rtb_ProcessName";
-			this.rtb_ProcessName.Size = new System.Drawing.Size(524, 26);
+			this.rtb_ProcessName.Size = new System.Drawing.Size(1110, 26);
 			this.rtb_ProcessName.TabIndex = 0;
 			this.rtb_ProcessName.Text = "notepad";
 			// 
@@ -55,10 +70,10 @@
 			// 
 			// btn_Stop
 			// 
-			this.btn_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.btn_Stop.Location = new System.Drawing.Point(461, 45);
+			this.btn_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn_Stop.Location = new System.Drawing.Point(1020, 45);
 			this.btn_Stop.Name = "btn_Stop";
-			this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+			this.btn_Stop.Size = new System.Drawing.Size(97, 23);
 			this.btn_Stop.TabIndex = 2;
 			this.btn_Stop.Text = "Стоп";
 			this.btn_Stop.UseVisualStyleBackColor = true;
@@ -67,7 +82,7 @@
 			// l_ProcessInfo
 			// 
 			this.l_ProcessInfo.AutoSize = true;
-			this.l_ProcessInfo.Location = new System.Drawing.Point(9, 85);
+			this.l_ProcessInfo.Location = new System.Drawing.Point(9, 266);
 			this.l_ProcessInfo.Name = "l_ProcessInfo";
 			this.l_ProcessInfo.Size = new System.Drawing.Size(57, 16);
 			this.l_ProcessInfo.TabIndex = 3;
@@ -83,11 +98,95 @@
 			this.proc_MyProcess.StartInfo.UserName = "";
 			this.proc_MyProcess.SynchronizingObject = this;
 			// 
+			// lv_ProcessList
+			// 
+			this.lv_ProcessList.AllowColumnReorder = true;
+			this.lv_ProcessList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lv_ProcessList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PID,
+            this.Process,
+            this.Base_priority,
+            this.Priority_class,
+            this.Start_time,
+            this.Total_CPU_time,
+            this.User_CPU_time,
+            this.Session_ID,
+            this.Affinity,
+            this.Threads});
+			this.lv_ProcessList.FullRowSelect = true;
+			this.lv_ProcessList.GridLines = true;
+			this.lv_ProcessList.HideSelection = false;
+			this.lv_ProcessList.Location = new System.Drawing.Point(12, 75);
+			this.lv_ProcessList.MultiSelect = false;
+			this.lv_ProcessList.Name = "lv_ProcessList";
+			this.lv_ProcessList.Size = new System.Drawing.Size(1106, 188);
+			this.lv_ProcessList.TabIndex = 4;
+			this.lv_ProcessList.UseCompatibleStateImageBehavior = false;
+			this.lv_ProcessList.View = System.Windows.Forms.View.Details;
+			this.lv_ProcessList.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lv_ProcessList_ItemSelectionChanged);
+			// 
+			// PID
+			// 
+			this.PID.Text = "PID";
+			this.PID.Width = 77;
+			// 
+			// Process
+			// 
+			this.Process.Text = "Process";
+			this.Process.Width = 150;
+			// 
+			// Base_priority
+			// 
+			this.Base_priority.Text = "Base priority";
+			this.Base_priority.Width = 129;
+			// 
+			// Priority_class
+			// 
+			this.Priority_class.Text = "Priority class";
+			this.Priority_class.Width = 100;
+			// 
+			// Start_time
+			// 
+			this.Start_time.Text = "Start time";
+			this.Start_time.Width = 82;
+			// 
+			// Total_CPU_time
+			// 
+			this.Total_CPU_time.Text = "Total CPU time";
+			this.Total_CPU_time.Width = 115;
+			// 
+			// User_CPU_time
+			// 
+			this.User_CPU_time.Text = "User CPU time";
+			this.User_CPU_time.Width = 111;
+			// 
+			// Session_ID
+			// 
+			this.Session_ID.Text = "Session ID";
+			this.Session_ID.Width = 94;
+			// 
+			// Affinity
+			// 
+			this.Affinity.Text = "Affinity";
+			this.Affinity.Width = 73;
+			// 
+			// Threads
+			// 
+			this.Threads.Text = "Threads";
+			this.Threads.Width = 93;
+			// 
+			// t_RefreshInfo
+			// 
+			this.t_RefreshInfo.Interval = 500;
+			this.t_RefreshInfo.Tick += new System.EventHandler(this.t_RefreshInfo_Tick);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(548, 357);
+			this.ClientSize = new System.Drawing.Size(1129, 481);
+			this.Controls.Add(this.lv_ProcessList);
 			this.Controls.Add(this.l_ProcessInfo);
 			this.Controls.Add(this.btn_Stop);
 			this.Controls.Add(this.btn_Start);
@@ -107,6 +206,18 @@
 		private System.Windows.Forms.Button btn_Stop;
 		private System.Windows.Forms.Label l_ProcessInfo;
 		private System.Diagnostics.Process proc_MyProcess;
+		private System.Windows.Forms.ListView lv_ProcessList;
+		private System.Windows.Forms.ColumnHeader PID;
+		private System.Windows.Forms.ColumnHeader Process;
+		private System.Windows.Forms.Timer t_RefreshInfo;
+		private System.Windows.Forms.ColumnHeader Priority_class;
+		private System.Windows.Forms.ColumnHeader Start_time;
+		private System.Windows.Forms.ColumnHeader Total_CPU_time;
+		private System.Windows.Forms.ColumnHeader User_CPU_time;
+		private System.Windows.Forms.ColumnHeader Session_ID;
+		private System.Windows.Forms.ColumnHeader Affinity;
+		private System.Windows.Forms.ColumnHeader Threads;
+		private System.Windows.Forms.ColumnHeader Base_priority;
 	}
 }
 
--- Processes/Form1.cs
+++ Processes/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,21 +14,20 @@ namespace Processes
 {
 	public partial class Form1 : Form
 	{
+		int index = 0;
+		int pid = 0;
+						
 		public Form1()
 		{
 			InitializeComponent();
 			InitProcess();
 		}
 
-		//void Form1_Closing(object sender, CancelEventArgs e)
-		//{
-		//	proc_MyProcess.CloseMainWindow(); 
-		//	proc_MyProcess.Close();
-		//}
 		void InitProcess()
 		{
 			AllignText();
 			proc_MyProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(rtb_ProcessName.Text);
+
 		}
 		void AllignText()
 		{ 
@@ -36,32 +36,119 @@ namespace Processes
 		}
 		void Info()
 		{
-			l_ProcessInfo.Text = "Process Info:\n";
-			l_ProcessInfo.Text += $"PID: {proc_MyProcess.Id}\n";
-			l_ProcessInfo.Text += $"BasePriority: {proc_MyProcess.BasePriority}\n";
-			l_ProcessInfo.Text += $"StartTime: {proc_MyProcess.StartTime}\n";
+			l_ProcessInfo.Text = "Process info:\n";
+			l_ProcessInfo.Text += $"Base priority:	{proc_MyProcess.BasePriority}\n";
+			l_ProcessInfo.Text += $"Priority class:	{proc_MyProcess.PriorityClass}\n";
+			l_ProcessInfo.Text += $"Start time:		{proc_MyProcess.StartTime}\n";
+			l_ProcessInfo.Text += $"Total CPU time:	{proc_MyProcess.TotalProcessorTime}\n";
+			l_ProcessInfo.Text += $"User  CPU time:	{proc_MyProcess.UserProcessorTime}\n";
+			l_ProcessInfo.Text += $"Session ID:		{proc_MyProcess.SessionId}\n";
+			l_ProcessInfo.Text += $"Affinity:		{proc_MyProcess.ProcessorAffinity}\n";
+			l_ProcessInfo.Text += $"Threads:		{proc_MyProcess.Threads.Count}\n";
 		}
 
 		private void btn_Start_Click(object sender, EventArgs e)
 		{
+
 			InitProcess();
 			proc_MyProcess.Start(); //Запуск процесса
-			Info();
+			ListViewItem lvi = new ListViewItem
+				(new string[] 
+					{ 
+						proc_MyProcess.Id.ToString(), 
+						proc_MyProcess.ProcessName, 
+						proc_MyProcess.BasePriority.ToString(),
+						proc_MyProcess.PriorityClass.ToString(),
+						proc_MyProcess.StartTime.ToString(),
+						proc_MyProcess.TotalProcessorTime.ToString(),
+						proc_MyProcess.UserProcessorTime.ToString(),
+						proc_MyProcess.SessionId.ToString(),
+						proc_MyProcess.ProcessorAffinity.ToString(),
+						proc_MyProcess.Threads.Count.ToString()
+					}
+				);
+			lv_ProcessList.Items.Add(lvi);
+			//Info();
+			t_RefreshInfo.Enabled = true;
 		}
 
 		private void btn_Stop_Click(object sender, EventArgs e)
 		{
 			try
 			{
-				proc_MyProcess.CloseMainWindow(); //Закрытие процесса
+				if (lv_ProcessList.FocusedItem == null)
+					index = lv_ProcessList.Items.Count - 1;
+				pid = Convert.ToInt32(lv_ProcessList.Items[index].SubItems[0].Text);
+				try
+				{
+					if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetProcessById(pid).ProcessName).Any())
+					{ 
+						proc_MyProcess = System.Diagnostics.Process.GetProcessById(pid);
+						proc_MyProcess.CloseMainWindow();
+					}
+				}
+				catch (Exception)
+				{}
+				lv_ProcessList.Items.RemoveAt(index);
 			}
 			catch (Exception) { }
-			proc_MyProcess.Close(); // Освобождает  ресурсы
+			if (lv_ProcessList.Items.Count <= 0) 
+				l_ProcessInfo.Text = "";
+			if (lv_ProcessList.Items.Count == 0)
+			{ 
+				proc_MyProcess.Close();
+				t_RefreshInfo.Enabled = false;
+			}
 		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 		{
-			btn_Stop_Click(sender, e);
+			while (lv_ProcessList.Items.Count != 0)
+			{ 
+				index = lv_ProcessList.Items.Count - 1;
+				btn_Stop_Click(sender, e);
+			}
+			t_RefreshInfo.Enabled = false;
+		}
+
+		private void lv_ProcessList_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+		{
+			try
+			{
+				index = lv_ProcessList.SelectedItems[0].Index;
+				pid = Convert.ToInt32(lv_ProcessList.SelectedItems[0].SubItems[0].Text);
+				proc_MyProcess = System.Diagnostics.Process.GetProcessById(pid);
+				//Info();
+			}
+			catch (Exception)
+			{ }
+		}
+		private void t_RefreshInfo_Tick(object sender, EventArgs e)
+		{
+			for (int i = 0; i < lv_ProcessList.Items.Count; i++)
+				SelectItem(i);
+		}
+
+		void SelectItem(int i)
+		{
+			try
+			{
+				int refresh_pid = Convert.ToInt32(lv_ProcessList.Items[i].SubItems[0].Text);
+				proc_MyProcess = System.Diagnostics.Process.GetProcessById(refresh_pid);
+
+				lv_ProcessList.Items[i].SubItems[0].Text = proc_MyProcess.Id.ToString();
+				lv_ProcessList.Items[i].SubItems[1].Text = proc_MyProcess.ProcessName;
+				lv_ProcessList.Items[i].SubItems[2].Text = proc_MyProcess.BasePriority.ToString();
+				lv_ProcessList.Items[i].SubItems[3].Text = proc_MyProcess.PriorityClass.ToString();
+				lv_ProcessList.Items[i].SubItems[4].Text = proc_MyProcess.StartTime.ToString();
+				lv_ProcessList.Items[i].SubItems[5].Text = proc_MyProcess.TotalProcessorTime.ToString();
+				lv_ProcessList.Items[i].SubItems[6].Text = proc_MyProcess.UserProcessorTime.ToString();
+				lv_ProcessList.Items[i].SubItems[7].Text = proc_MyProcess.SessionId.ToString();
+				lv_ProcessList.Items[i].SubItems[8].Text = proc_MyProcess.ProcessorAffinity.ToString();
+				lv_ProcessList.Items[i].SubItems[9].Text = proc_MyProcess.Threads.Count.ToString();
+			}
+			catch (Exception)
+			{ }
 		}
 	}
 }
